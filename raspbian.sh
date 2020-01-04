71046e8d1d20d49147f085190212f8c211ac90c2
necro-nemesis <necro_nemesis@hotmail.com>
2020-01-04 06:46:50 -0500

    install dependencies

--- raspbian.sh
+++ raspbian.sh
@@ -1,6 +1,6 @@
-UPDATE_URL="https://raw.githubusercontent.com/necro-nemesis/Lokiap-webgui/master/"
-wget -q ${UPDATE_URL}/installers/common.sh -O /tmp/raspapcommon.sh
-source /tmp/raspapcommon.sh && rm -f /tmp/raspapcommon.sh
+UPDATE_URL="https://raw.githubusercontent.com/necro-nemesis/SNApp-Pi-Host/master/"
+wget -q ${UPDATE_URL}/installers/common.sh -O /tmp/pihostcommon.sh
+source /tmp/pihostcommon.sh && rm -f /tmp/pihostcommon.sh
 
 function update_system_packages() {
     install_log "Updating sources"
@@ -14,20 +14,7 @@ function install_dependencies() {
     curl -s https://deb.imaginary.stream/public.gpg | sudo apt-key add -
     echo "deb https://deb.imaginary.stream $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/imaginary.stream.list
     sudo apt-get update
-    sudo yes | apt-get install lighttpd $php_package git hostapd dnsmasq vnstat resolvconf lokinet || install_error "Unable to install dependencies"
+    sudo yes | apt-get install git bind-utils resolvconf lokinet || install_error "Unable to install dependencies"
 }
 
-#Remove NetworkManager and install dhcpd if required.
-
-function check_for_networkmananger() {
-  install_log "Checking for NetworkManager"
-  echo "Checking for Network Manager"
-    if [ -f /lib/systemd/system/network-manager.service ]; then
-  echo "Network Manager found. Replacing with DHCPCD"
-        sudo apt-get -y purge network-manager
-        sudo apt-get -y install dhcpcd5
-    fi
-
-}
-
-install_raspap
+install_pihost
