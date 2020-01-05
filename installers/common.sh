102a4c7292cf75ce04ae6bf62ed191ef6ace46d6
necro-nemesis <necro_nemesis@hotmail.com>
2020-01-04 23:28:00 -0500

    test

--- installers/common.sh
+++ installers/common.sh
@@ -118,7 +118,7 @@ function install_complete() {
 
 		sudo systemctl stop lokinet
 
-		sed '$ a\ keyfile=/var/lib/lokinet/snappkey.private' /var/lib/lokinet/lokinet.ini
+		sed -e '$ a\ keyfile=/var/lib/lokinet/snappkey.private' /var/lib/lokinet/lokinet.ini
 		#restart Lokinet
 
 		sudo systemctl start lokinet
